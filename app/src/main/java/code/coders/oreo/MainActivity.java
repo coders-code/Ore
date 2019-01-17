e85f309e6ed0eb7d194712983c3d3570574eb7df
Coder <sonalji.2014@gmail.com>
2019-01-17 11:57:29 +0530

    Initial commit

---
+++ app/src/main/java/code/coders/oreo/MainActivity.java
@@ -0,0 +1,12 @@
+package code.coders.oreo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
